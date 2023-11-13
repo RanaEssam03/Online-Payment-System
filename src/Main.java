ad982c9ee0fa336af5f20c5198708edac40461b0
mariam1h <96283723+mariam1h@users.noreply.github.com>
2023-11-13 02:51:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import models.Account;
 import services.Authentication;
 
 public class Main {
+    private static Account currentUser;
+
     public static void main(String[] args) {
         // Create an instance of the Authentication class
         Authentication authentication = new Authentication();
@@ -15,52 +17,52 @@ public class Main {
 
         Account account2 = new Account("Nour", "nour@gmail.com", "01126134834", "5678", 500.0);
         authentication.addAccount(account2);
+        while (true) {
+            // Prompt the user to choose between login, logout, or exit
+            System.out.println("Choose an option:");
+            System.out.println("1. Login");
+            System.out.println("2. Logout");
+            System.out.println("3. Exit");
 
-        // Prompt the user to choose between login and logout
-        System.out.println("Choose an option:");
-        System.out.println("1. Login");
-        System.out.println("2. Logout");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    // Login case
+                    System.out.println("Enter login credentials:");
+                    System.out.print("Username: ");
+                    String username = scanner.next();
+                    System.out.print("Password: ");
+                    String password = scanner.next();
 
-        int choice = scanner.nextInt();
+                    Account loginAttempt = authentication.login(username, password);
+                    if (loginAttempt != null) {
+                        currentUser = loginAttempt;
+                        System.out.println("Login successful. Welcome, " + currentUser.getName() + "!");
+                    } else {
+                        System.out.println("Incorrect login attempt. No account found with the given credentials.");
+                    }
+                    break;
 
-        switch (choice) {
-            case 1:
-                // Login case
-                System.out.println("Enter login credentials:");
-                System.out.print("Username: ");
-                String username = scanner.next();
-                System.out.print("Password: ");
-                String password = scanner.next();
+                case 2:
+                    // Logout case
+                    if (currentUser != null) {
+                        authentication.logout();
+                        System.out.println("Logout successful for " + currentUser.getName() + ".");
+                        currentUser = null;
+                    } else {
+                        System.out.println("No one is currently logged in. Cannot logout.");
+                    }
+                    break;
 
-                Account loginAttempt = authentication.login(username, password);
-                if (loginAttempt != null) {
-                    System.out.println("Login successful. Welcome, " + loginAttempt.getName() + "!");
-                } else {
-                    System.out.println("Incorrect login attempt. No account found with the given credentials.");
-                }
-                break;
+                case 3:
+                    // Exit the program
+                    System.out.println("Exiting the program. Goodbye!");
+                    scanner.close();
+                    System.exit(0);
 
-            case 2:
-                // Logout case
-                if (authentication.isLoggedIn()) {
-                    authentication.logout();
-                    System.out.println("Logout successful.");
-                } else {
-                    System.out.println("No one is currently logged in. Cannot logout.");
-                }
-                break;
-
-            default:
-                System.out.println("Invalid choice. Please choose 1 or 2.");
+                default:
+                    System.out.println("Invalid choice. Please choose 1, 2, or 3.");
+            }
         }
-        // Check if someone is logged in after login/logout
-        if (authentication.isLoggedIn()) {
-            System.out.println("Someone is currently logged in.");
-            System.out.println("Logged-in account: " + authentication.getLoggedInAccount().getName());
-        } else {
-            System.out.println("No one is currently logged in.");
-        }
-        // Close the scanner
-        scanner.close();
     }
 }
